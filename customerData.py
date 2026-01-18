5da2282dc3194d139c9c0e256ff64dd0aaf324af
Evelyn Bushell <155328177+Eveybushell@users.noreply.github.com>
2026-01-17 20:44:35 -0800

    Partial solution

--- customerData.py
+++ customerData.py
@@ -1,7 +1,30 @@
 def mergeData (customerData1, customerData2, m, n):
-    count = 0
-    for i in range(m+n):
-        if i >= n:
-            customerData1[i] = customerData2[count]
-            count += 1
+    # count = 0
+    # if n == 0:
+    #     return customerData1
+    # for i in range(n,m+n):
+    #     customerData1[i] = customerData2[count]
+    #     count += 1
+    # # Sorting causes solution to be O(nlogn)
+    # customerData1.sort()
+    # return customerData1
+    
+    i = m - 1        # last valid element in customerData1
+    j = n - 1        # last element in customerData2
+    k = m + n - 1    # last position in customerData1
+
+    while i >= 0 and j >= 0:
+        if customerData1[i] > customerData2[j]:
+            customerData1[k] = customerData1[i]
+            i -= 1
+        else:
+            customerData1[k] = customerData2[j]
+            j -= 1
+        k -= 1
+
+    # If anything remains in customerData2
+    while j >= 0:
+        customerData1[k] = customerData2[j]
+        j -= 1
+        k -= 1
     return customerData1
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from customerData import mergeData
 
 class MergeTest (unittest.TestCase):
     
-    def sampleTest1 (self):
+    def testSample1 (self):
         self.assertEqual(mergeData([101,104,107,0,0,0], [102,105,108], 3, 3), [101,102,104,105,107,108])
 
 if __name__ == "__main__":
